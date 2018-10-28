2cad1db79af1f9fdd615153002f71bae813df23c
farshiddk <42038507+farshiddk@users.noreply.github.com>
2018-10-28 04:16:10 +0330

    Update base.lua

--- base.lua
+++ base.lua
@@ -18,12 +18,12 @@ reload()
 
 function tdcli_update_callback(data)
   tabchi.update(data, tabchi_id)
-  if data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/reload" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
+  if data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "بارگزاری مجدد ربات" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
     reload()
-    tdcli.sendMessage(data.message_.chat_id_, 0, 1, "*Bot Reloaded*", 1, "md")
-  elseif data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/gitpull" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
+    tdcli.sendMessage(data.message_.chat_id_, 0, 1, "*ربات مجدد بارگزاری شد *", 1, "md")
+  elseif data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "بروزرسانی" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
     io.popen("git fetch --all && git reset --hard origin/master && git pull origin master"):read("*all")
     reload()
-    tdcli.sendMessage(data.message_.chat_id_, 0, 1, "*Updates Received And Bot Reloaded*", 1, "md")
+    tdcli.sendMessage(data.message_.chat_id_, 0, 1, "*ربات اپدیت ها را چک کرد و بارگزاری مجدد شد*", 1, "md")
   end
 end
